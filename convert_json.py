7bbf16e1cce319ebf28a713e1ce8d883ee4c5a41
Alex Patterson <alexpatt_git@pm.me>
2025-03-30 20:32:34 +0100

    feat[convert_json]: write latex preamble to output file

--- convert_json.py
+++ convert_json.py
@@ -10,14 +10,22 @@ with open(filename_input, 'r') as json_file:
 
     with open(filename_output, 'w') as output_file:
 
+        # latext preamble
+        output_file.write((
+            "\\documentclass[12pt]{article}\n"
+            "\\usepackage[english]{babel}\n"
+            "\\usepackae[utf8]{inputenc}\n"
+            "\\usepackage{blindtext}\n"
+        ))
+
         # Name/Title
-        output_file.write(cv_data['name'])
+      #  output_file.write(cv_data['name'])
 
         # Locaton
-        output_file.write(f"\n{cv_data['location']}")
+      #  output_file.write(f"\n{cv_data['location']}")
 
         # Phone
-        output_file.write(f"\n{cv_data['phone']}")
+      #  output_file.write(f"\n{cv_data['phone']}")
 
         # Email
-        output_file.write(f"\n{cv_data['email']}")
+   #     output_file.write(f"\n{cv_data['email']}")
